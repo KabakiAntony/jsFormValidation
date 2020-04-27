6e7887c945ca5e1e784aa20963733032c7b1e5c3
Kabaki Antony <kabaki.antony@gmail.com>
2020-04-27 22:30:38 +0300

    Added more validation

--- validate.js
+++ validate.js
@@ -1,7 +1,52 @@
 /* here is the javascript that we are going to use to validate the various inputs */
 let submitForm = document.getElementById("form");
 
-// declare various functions below here.
+
+submitForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirm-password").value; 
+    /*
+    the reason am returning a false in all
+    my ifs is to prevent the submit from 
+    acting further down on my code till
+    all actions are true this prevents 
+    fetching with invalid data all
+    input data has to be valid first
+    before going on to fetch.
+    */ 
+
+    if(!isEmail(email)){
+        document.getElementById("email").style.backgroundColor = "rgba(204, 120, 120, 0.513)";
+        document.getElementById("email").style.border = "0";
+        document.getElementById("error-email").innerHTML = "enter a valid email !";
+        return false;
+    }
+    if(!isValidPassword(password)){
+        document.getElementById("password").style.backgroundColor = "rgba(204, 120, 120, 0.513)";
+        document.getElementById("password").style.border = "0";
+        document.getElementById("error-password").innerHTML = "password does not meet requirements !";
+        return false;
+    }
+    if(!doPasswordsMatch(password,confirmPassword)){
+        document.getElementById("confirm-password").style.backgroundColor = "rgba(204, 120, 120, 0.513)";
+        document.getElementById("confirm-password").style.border = "0";
+        document.getElementById("error-confirm-password").innerHTML = "passwords do not match !";
+        return false;
+    }
+    console.log("This should only show when all above is true");    
+});
+
+
+// in js when dealing with regex declare flags eg "igm"
+/*
+i - insensitive
+g - global
+m - multiline
+there are others  but these flags determine how your string is parsed
+*/
 function isEmail(my_email){
     let emailRegex =/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
     return my_email.match(emailRegex);
@@ -13,19 +58,5 @@ function isValidPassword(my_password){
 function doPasswordsMatch(passOne,passTwo){
     return (passOne === passTwo);
 }
-submitForm.addEventListener('submit',(e)=>{
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let confirmPassword = document.getElementById("confirm-password").value;
-    
-    e.preventDefault();
-    if(!isEmail(email)){
-        document.getElementById("email").style.borderColor = "red";
-    }
-    if(!isValidPassword(password)|| !doPasswordsMatch(password,confirmPassword)){
-        document.getElementById("password").style.borderColor = "red";
-    }
-});
-
 
 
