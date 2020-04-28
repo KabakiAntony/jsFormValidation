8aeab216a4936fb7dae882fd6d694af5ee5ab1d0
Kabaki Antony <kabaki.antony@gmail.com>
2020-04-28 13:42:36 +0300

    finalized all validations

--- validate.js
+++ validate.js
@@ -1,13 +1,12 @@
 /* here is the javascript that we are going to use to validate the various inputs */
 let submitForm = document.getElementById("form");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirm-password");
 
 
 submitForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let confirmPassword = document.getElementById("confirm-password").value; 
+    e.preventDefault();  
     /*
     the reason am returning a false in all
     my ifs is to prevent the submit from 
@@ -18,19 +17,48 @@ submitForm.addEventListener('submit',(e)=>{
     before going on to fetch.
     */ 
 
-    if(!isEmail(email)){
+    if(!isEmail(email.value)){
         document.getElementById("error-email").innerHTML = "Enter a valid email !";
         return false;
     }
-    if(!isValidPassword(password)){
+    if(!isValidPassword(password.value)){
         document.getElementById("error-password").innerHTML = "Password does not meet requirements !";
         return false;
     }
-    if(!doPasswordsMatch(password,confirmPassword)){
-        document.getElementById("error-confirm-password").innerHTML = "Passwords do not match !";
-        return false;
-    }
     console.log("This should only show when all above is true");    
+});
+email.addEventListener('input',(e)=>{
+    if(isEmail(email.value)){
+        document.getElementById("error-email").innerHTML = ""; 
+    }
+    else{
+        document.getElementById("error-email").innerHTML = "Enter a valid email !";
+    }
+});
+password.addEventListener('input',(e)=>{
+    if(isValidPassword(password.value))
+    {
+        document.getElementById("error-password").innerHTML = "";
+    }else{
+        document.getElementById("error-password").innerHTML = 
+        `Password should contain atleast 
+        1 uppercase character,
+        1 lowercase character,
+        1 number, 
+        1 special character,
+        atleast 6 characters
+        & not more than 20.`;
+    }
+});
+confirmPassword.addEventListener('input',(e)=>{
+    if(doPasswordsMatch(password.value,confirmPassword.value)){
+        document.getElementById("password").style.backgroundColor= "rgb(232,240,254)";
+        document.getElementById("confirm-password").style.backgroundColor= "rgb(232,240,254)";
+        document.getElementById("error-confirm-password").innerHTML = "";
+
+    }else{
+        document.getElementById("error-confirm-password").innerHTML = "Passwords do not match !";
+    }
 });
 
 
